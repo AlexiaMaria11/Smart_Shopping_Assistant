68c4774df34afcda5847e849576b886cb10121a0
Ale <oprtitamariaalexia@gmail.com>
2026-04-28 11:29:56 +0300

    Added reguired annotations

--- SmartShoppingAssistant.BusinessLogic/DTOs/ProductUpdateDTO.cs
+++ SmartShoppingAssistant.BusinessLogic/DTOs/ProductUpdateDTO.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SmartShoppingAssistant.BusinessLogic.DTOs
 {
     public class ProductUpdateDTO
     {
+        [Required]
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string ImageUrl { get; set; } = null!;
+        [Required]
+        [Range(0.01, double.MaxValue)]
         public decimal Price { get; set; }
     }
 }
